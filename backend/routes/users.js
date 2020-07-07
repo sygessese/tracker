e60d3186d593624fe0077fe330073f2b8f77cc9f
Selam Gessese <sygessese@gmail.com>
2020-07-06 20:18:41 -0700

    tracker created

---
+++ backend/routes/users.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const User = require("../models/user.model");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const NewUser = new User({ username });
+  NewUser.save()
+    .then(() => res.json("User added"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/tracker", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log("Server running on port ", port);
+});
